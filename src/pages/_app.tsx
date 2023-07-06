31e8ba953cefda98ad72472e6fcf59bad411d24c
Jamie Stegman <hello@jamiestegman.com>
2023-07-06 11:37:51 +1000

    Add theme-provider.tsx component from next-themes, move Inter font family to _app.tsx for global styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ThemeProvider } from "@/components/theme/theme-provider";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+            <style jsx global>{`
+                html {
+                    font-family: ${inter.style.fontFamily};
+                    letter-spacing: -0.025em;
+                }
+            `}</style>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
